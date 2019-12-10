725b2be9a5d7a0789b414a889653d6e72a3b4882
JTMT1997 <jtumbur11@gmail.com>
2019-12-10 16:37:10 +0700

    membuat cart

---
+++ cart.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function(){
+        $("#showtable").click(function(){
+          $("#tables").fadeOut(3000);
+         });
+       $("#hidetable").click(function(){
+         $("#tables").fadeIn(3000);
+          });
+      });
